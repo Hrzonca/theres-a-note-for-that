560b79fae3f6d2926c022b959faeb606c493ea45
Haleigh Rzonca <hgielah@gmail.com>
2022-11-02 14:37:27 -0700

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 //change the port but idk what yet
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
